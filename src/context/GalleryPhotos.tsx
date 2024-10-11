2fed94e015ad325f38688337870d2ef2319a0f01
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-10-11 20:34:01 +0200

    New magic photos

--- src/context/GalleryPhotos.tsx
+++ src/context/GalleryPhotos.tsx
@@ -8,7 +8,8 @@ import p7 from "../assets/ZdjęciaPsów/7.jpeg";
 import p8 from "../assets/ZdjęciaPsów/8.jpeg";
 import p9 from "../assets/ZdjęciaPsów/9.jpeg";
 import p10 from "../assets/ZdjęciaPsów/10.jpeg";
+import p11 from "../assets/ZdjęciaPsów/kek.jpg";
 
-const photos = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10];
+const photos = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11];
 
 export default photos;
