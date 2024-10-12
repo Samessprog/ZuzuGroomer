e95f19b1f49f5f954f2fcc9e4aa4a058fbfc2a21
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-10-12 15:36:53 +0200

    New Photos gallery / better strat

--- src/context/GalleryPhotos.tsx
+++ src/context/GalleryPhotos.tsx
@@ -4,10 +4,9 @@ import p3 from "../assets/ZdjęciaPsów/3.jpeg";
 import p4 from "../assets/ZdjęciaPsów/4.png";
 import p5 from "../assets/ZdjęciaPsów/5.png";
 import p6 from "../assets/ZdjęciaPsów/6.png";
-import p7 from "../assets/ZdjęciaPsów/7.jpeg";
-import p8 from "../assets/ZdjęciaPsów/8.jpeg";
-import p9 from "../assets/ZdjęciaPsów/9.jpeg";
-import p10 from "../assets/ZdjęciaPsów/10.jpeg";
+import p8 from "../assets/C.jpg";
+import p9 from "../assets/B.jpg";
+import p10 from "../assets/A.jpg";
 import p11 from "../assets/ZdjęciaPsów/kek.jpg";
 
 const photos = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11];
