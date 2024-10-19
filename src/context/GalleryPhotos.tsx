51662b72e3d066cf72f0410c13c2691987e00eaf
Samessprog <100785560+Samessprog@users.noreply.github.com>
2024-10-19 18:14:17 +0200

    New Gallery photos layout

--- src/context/GalleryPhotos.tsx
+++ src/context/GalleryPhotos.tsx
@@ -1,7 +1,7 @@
-import p8 from "../assets/ZdjęciaPsów/C.jpg";
-import p9 from "../assets/ZdjęciaPsów/B.jpg";
-import p10 from "../assets/ZdjęciaPsów/A.jpg";
-import p11 from "../assets/ZdjęciaPsów/kek.jpg";
+import p8 from "../assets/ZdjęciaPsów/C.webp";
+import p9 from "../assets/ZdjęciaPsów/B.webp";
+import p10 from "../assets/ZdjęciaPsów/A.webp";
+import p11 from "../assets/ZdjęciaPsów/kek.webp";
 
 const photos = [p8, p9, p10, p11];
 export default photos;
